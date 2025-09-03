a894f653bd8fcf8986177b1cd625b8660f8e7851
aasthamahajan19 <aasthamahajan1928@gmail.com>
2025-09-03 21:51:33 +0530

    ques5d.cpp

--- ques5d.cpp
+++ ques5d.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int n=4;
-    int up[10]={1,2,3,4,5,6,7,8,9,10};
-    int k=0;
+    int n;
+    cin >> n;
+    int size=n*(n+1)/2;
+    int a[size], k=0, u[n][n];
+    for(int i=0;i<size;i++) cin >> a[i];
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            if(i<=j) cout<<up[k++]<<" ";
-            else cout<<0<<" ";
+            if(i<=j) {
+                u[i][j]=a[k++];
+            }
+            else {
+                u[i][j]=0;
+            }
         }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++) cout<<u[i][j]<<" ";
         cout<<endl;
     }
 }
