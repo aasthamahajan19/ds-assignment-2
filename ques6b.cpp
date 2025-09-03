59df2f5eca554858ee812f88aa9b2271b71c1d70
aasthamahajan19 <aasthamahajan1928@gmail.com>
2025-09-03 21:45:19 +0530

    ques6b.cpp

--- ques6b.cpp
+++ ques6b.cpp
@@ -1,21 +1,56 @@
 #include <iostream>
 using namespace std;
-struct Element {int r,c,v;};
+
 int main() {
-    Element a[2]={{0,0,5},{1,2,10}};
-    Element b[2]={{0,0,3},{1,1,4}};
-    Element c[4]; int k=0;
-    int i=0,j=0;
-    while(i<2 && j<2){
-        if(a[i].r==b[j].r && a[i].c==b[j].c){
-            c[k]={a[i].r,a[i].c,a[i].v+b[j].v}; i++; j++; k++;
-        } else if(a[i].r<b[j].r || (a[i].r==b[j].r && a[i].c<b[j].c)){
-            c[k++]=a[i++];
-        } else {
-            c[k++]=b[j++];
+    int r,c, A[100][3], B[100][3], C[200][3];
+    int mat1[10][10], mat2[10][10];
+
+    cout << "Enter rows and cols: ";
+    cin >> r >> c;
+
+    cout << "Enter Matrix A:\n";
+    int k=1;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin >> mat1[i][j];
+            if(mat1[i][j]!=0){
+                A[k][0]=i; A[k][1]=j; A[k][2]=mat1[i][j]; k++;
+            }
         }
     }
-    while(i<2) c[k++]=a[i++];
-    while(j<2) c[k++]=b[j++];
-    for(int x=0;x<k;x++) cout<<c[x].r<<" "<<c[x].c<<" "<<c[x].v<<endl;
+    A[0][0]=r; A[0][1]=c; A[0][2]=k-1;
+
+    cout << "Enter Matrix B:\n";
+    int l=1;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cin >> mat2[i][j];
+            if(mat2[i][j]!=0){
+                B[l][0]=i; B[l][1]=j; B[l][2]=mat2[i][j]; l++;
+            }
+        }
+    }
+    B[0][0]=r; B[0][1]=c; B[0][2]=l-1;
+
+    // Addition
+    int i=1,j=1,m=1;
+    C[0][0]=r; C[0][1]=c; C[0][2]=0;
+    while(i<A[0][2]+1 && j<B[0][2]+1){
+        if(A[i][0]<B[j][0] || (A[i][0]==B[j][0] && A[i][1]<B[j][1]))
+            {C[m][0]=A[i][0]; C[m][1]=A[i][1]; C[m][2]=A[i][2]; i++; m++;}
+        else if(B[j][0]<A[i][0] || (A[i][0]==B[j][0] && B[j][1]<A[i][1]))
+            {C[m][0]=B[j][0]; C[m][1]=B[j][1]; C[m][2]=B[j][2]; j++; m++;}
+        else{
+            int sum=A[i][2]+B[j][2];
+            if(sum!=0){C[m][0]=A[i][0]; C[m][1]=A[i][1]; C[m][2]=sum; m++;}
+            i++; j++;
+        }
+    }
+    while(i<=A[0][2]){C[m][0]=A[i][0]; C[m][1]=A[i][1]; C[m][2]=A[i][2]; i++; m++;}
+    while(j<=B[0][2]){C[m][0]=B[j][0]; C[m][1]=B[j][1]; C[m][2]=B[j][2]; j++; m++;}
+    C[0][2]=m-1;
+
+    cout<<"\nA+B (triplet):\nRow Col Val\n";
+    for(int x=0;x<m;x++)
+        cout<<C[x][0]<<"   "<<C[x][1]<<"   "<<C[x][2]<<endl;
 }
