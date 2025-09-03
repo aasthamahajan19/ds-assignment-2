58bb5ede1fab35aa85e647923c841e7a0f0790ef
aasthamahajan19 <aasthamahajan1928@gmail.com>
2025-09-03 21:54:16 +0530

    ques5a.cpp

--- ques5a.cpp
+++ ques5a.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int n=4;
-    int diag[4]={1,2,3,4};
+    int n;
+    cin >> n;
+    int a[n], d[n][n];
+    for(int i=0;i<n;i++) {
+        cin >> a[i];
+    }
+    int k=0;
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            if(i==j) cout<<diag[i]<<" ";
-            else cout<<0<<" ";
+            if(i==j)
+            {
+                d[i][j]=a[k++];
+            }
+            else {
+                d[i][j]=0;
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++) {
+            cout<<d[i][j]<<" ";
         }
         cout<<endl;
     }
